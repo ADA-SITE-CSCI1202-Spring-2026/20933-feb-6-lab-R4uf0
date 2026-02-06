947d496e5168ebf205ceb9a2e794f9bba1732292
Rauf Asadli <rasadli20271@ada.edu.az>
2026-02-06 13:57:14 +0400

    task_done

---
+++ src/Time.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Time {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of seconds: ");
+        int totalSeconds = scanner.nextInt();
+
+        int hours = totalSeconds / 3600;
+        int remainingSeconds = totalSeconds % 3600;
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+
+        System.out.println(totalSeconds + " seconds = " + hours + " hours, " +
+                minutes + " minutes, " + seconds + " seconds.");
+    }
+}
